4180f653391b574438cc33fb5cd45f71aabfd8f8
saymeoow <meowizy94@gmail.com>
2022-12-04 18:29:50 +0300

    hw 19

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main),
-    path('about/', views.about),
-    path('support/', views.support),
+    path('about', views.about, name='about'),
+    path('support', views.support, name='support'),
+    path('smartphones', views.Phone, name='smartphones')
 ]
